88eab8e64e907773fad30aea8a46593a7e7b3947
VictorCarreira <carreiravr@gmail.com>
2014-02-03 17:59:10 -0200

    adicionei o GEOLOGVS.py

---
+++ GEOLOGVS.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 11:51:35 2014
+
+@author: Victor Carreira
+"""
+
+print "Bem vindo ao GEOLOVM! O que deseja saber?"
+nome= raw_input()
+print "Minha pesquisa, ao longo das Eras Geológicas, resultou em:" +nome+ "->é um aglomerado de minerais" 
+nome=raw_input()
